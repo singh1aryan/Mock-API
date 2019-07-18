73f918fa0b93259279a0cc41b3d385d7dd16d3bc
Aryan Singh <Aryan.Singh@arrow.com>
2019-07-18 13:21:48 -0600

    update API

--- node/api/routes/listRoute.js
+++ node/api/routes/listRoute.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = function(app) {
-  var todoList = require('../controllers/todoListController');
+  var todoList = require('../controllers/listController');
 
   // todoList Routes
   app.route('/tasks')
--- node/server.js
+++ node/server.js
@@ -1,6 +1,22 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/listRoutes'), //created model loading here
+  bodyParser = require('body-parser');
+  
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb'); 
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/listRoutes'); //importing route
+routes(app); //register the route
+
 
 app.listen(port);
 
